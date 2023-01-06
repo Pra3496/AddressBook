ae02d7e13f27da88ccb9585a18ece02dcba4fcea
Pranav Waghmare <piyushw55@gmail.com>
2023-01-06 17:15:00 +0530

    version 1

---
+++ AddressBook/Person.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay10
+{
+    public class Person         //Creating class
+    {
+        public string FirstName;
+        public string LastName;
+        
+        public static List<Person> people = new List<Person>();
+      
+        //Creating contructor for address book
+        public Person() { }
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            
+        }
+        public static void AddPerson(int numContact)          //Creating method for taking user inputs
+        {
+            for (int i = 1; i <= numContact; i++)
+            {
+
+                Person person = new Person();
+                Console.CursorLeft = 70;
+                Console.WriteLine("----{0}----",i);
+                Console.CursorLeft = 70;
+                Console.Write("Enter First Name:     ");
+                person.FirstName = Console.ReadLine();
+                Console.CursorLeft = 70;
+                Console.Write("Enter Last Name:      ");
+                person.LastName = Console.ReadLine();
+                people.Add(person);
+            }
+        }
+        public static void PrintPerson()            //Creating method to display persons details
+        {
+            int no = 1;
+
+            Console.Write("\n");
+
+            Console.CursorLeft = 73;
+            Console.Write("**************>| All Contacts Present in Address Book |<*************", Console.CursorLeft);
+            
+            Console.Write("\n\n");
+
+            
+            foreach (Person per in people)
+            {
+                
+                Console.CursorLeft = 74;
+                Console.WriteLine("-------------------- {0} -------------------",no);
+                Console.CursorLeft = 74;
+                Console.WriteLine("First Name:   "+per.FirstName);
+                Console.CursorLeft = 74;
+                Console.WriteLine("Last Name:    " +per.LastName);
+                Console.CursorLeft = 74;
+                Console.WriteLine("------------------------------------------");
+                no++;
+            }
+            Console.Write("\n");
+            Console.CursorLeft = 73;
+            Console.Write("******************************>| END |<******************************", Console.CursorLeft);
+            Console.Write("\n");
+        }
+        public static void editContact(string name, int opt)        //Creating Method to edit contact
+        {
+            bool input = false;
+           
+            foreach (Person person in people)
+            {
+                if (person.FirstName == name)
+                {
+                    Console.CursorLeft = 70;
+                    Console.WriteLine("MATCH FOUND !!!", Console.CursorLeft);
+                    Console.WriteLine();
+                    Console.CursorLeft = 70;
+                    Console.WriteLine("Current Details");
+                    Console.CursorLeft = 70;
+                    Console.WriteLine(person.FirstName);
+                    Console.CursorLeft = 70;
+                    Console.WriteLine(person.LastName);
+
+                    input = true;
+                    switch (opt)         //Switch case for editing parameter
+                    {
+                        case 1:
+                            Console.CursorLeft = 70;
+                            Console.Write("Enter current first name:   ");
+                            string currentFirstName = Convert.ToString(Console.ReadLine());
+                            person.FirstName = currentFirstName;
+                            Console.CursorLeft = 88;
+                            Console.WriteLine("CHANGE DONE...");
+                            break;
+                        case 2:
+                            Console.CursorLeft = 70;
+                            Console.Write("Enter current last name:    ");
+                            string currentLastName = Convert.ToString(Console.ReadLine());
+                            person.LastName = currentLastName;
+                            Console.WriteLine("CHANGE DONE...");
+                            break;
+                        
+                        default:
+                            Console.Write("Please enter valid option");
+                            break;
+                    }
+                }
+                
+            }
+            if (input == false)
+            {
+                Console.CursorLeft = 70;
+                Console.WriteLine("Initials not found");
+            } 
+        }
+        public static void deleteContact(string name)      //Creating method for delete contact
+        {
+            bool flag = false;
+            foreach (Person person1 in people)
+            {
+                if (person1.FirstName == name)
+                {
+                    flag = true;
+                    people.Remove(person1);
+                    Console.CursorLeft = 70;
+                    Console.WriteLine("The contact is successfully removed");
+                    break;
+                }
+            }
+            if (flag == false)
+            {
+                Console.CursorLeft = 70;
+                Console.WriteLine("The contact is not found");
+            }
+                
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace AddressBookDay10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // setting the window size
+
+
+            // setting buffer size of console
+
+
+            // using the method
+            
+
+            // Get the CursorLeft position
+            
+
+
+            Console.SetWindowSize(210,50);
+
+
+            
+            //Start
+            Main_Menu:
+                        Console.Write("\n\n\n\n");
+                        Console.CursorLeft = 70;
+                        Console.WriteLine("__________________________________________________________________");
+                        Console.WriteLine();
+                        Console.CursorLeft = 90;
+                        Console.WriteLine("WELCOME TO ADDRESS BOOK PROGRAM",Console.CursorLeft);
+                        
+                        Console.CursorLeft = 70;
+                        Console.Write("__________________________________________________________________", Console.CursorLeft);
+                        Console.Write("\n\n");
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  1: Ability to create Contacts in AddressBook  |", Console.CursorLeft);
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  2: Ability to add new coontact to AddressBook |",Console.CursorLeft);
+
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  3: Edit contact in AddressBook                |", Console.CursorLeft);
+
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  4: Remove contact from AddressBook            |", Console.CursorLeft);
+
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  5: Display All Contacts                       |", Console.CursorLeft);
+
+                        Console.CursorLeft = 80;
+                        Console.WriteLine("|  6: Exit from Application                      |", Console.CursorLeft);
+
+
+                        Console.WriteLine();
+                        Console.CursorLeft = 70;
+                        Console.Write("__________________________________________________________________");
+                        Console.Write("\n");
+                        Console.CursorLeft = 70;
+                        Console.Write("Enter Your Choice : ", Console.CursorLeft);
+                        int option = Convert.ToInt32(Console.ReadLine());   
+                        //Taking user input
+                        //Calling methods using Switch case for UC1,UC2, UC3, UC4 and UC5
+                        Console.Clear();
+            
+                        switch (option)
+                        {
+                            case 1:
+                                {
+                                    Console.Write("\n\n\n\n");
+                                    
+                                    Console.CursorLeft = 87;
+                                    Console.Write("CREATE CONTACTS IN ADDRESS-BOOK");
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+
+
+                                    Console.Write("Enter the number of contact you wanted to Make : ",Console.CursorLeft);
+                                    int numContact = Convert.ToInt32(Console.ReadLine());
+                                    Person.AddPerson(numContact);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 90;
+                                    Console.WriteLine("Successfully Added Contact...!!");
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Press any key to Main menu..");
+                                    
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto Main_Menu;
+                                }
+            
+                               
+                            case 2:
+                                {
+                                    Console.Write("\n\n\n\n");
+                      
+                                    Console.CursorLeft = 98;
+                                    Console.Write("ADD CONTACT", Console.CursorLeft);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Enter the first name of contact you wanted to Make : ");
+                                    int numContact = Convert.ToInt32(Console.ReadLine());
+                                    Person.AddPerson(numContact);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 90;
+                                    Console.WriteLine("Successfully Added Contact...!!");
+                                    Console.Write("\n");
+                                    
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Press any key to Main menu..");
+                                    
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto Main_Menu;
+                    }
+                                
+                            case 3:
+                                {
+                                    Console.Write("\n\n\n\n");
+                                    
+                                    Console.CursorLeft = 87;
+                                    Console.Write("SEARCH or EDIT IN ADDRESS BOOK");
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                        
+                                    Console.Write("\n\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("|  1: Edit for Firstname  |");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("|  2: Edit for LastName   |");
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Enter your choice :  ");
+                                    int opt = Convert.ToInt32(Console.ReadLine());
+                                    
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Enter the Initials(name or last):   ");
+                                    string name = Convert.ToString(Console.ReadLine());
+                                    Person.editContact(name,opt);
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Press any key to Main menu..");
+
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto Main_Menu;
+                                }
+                                
+                            case 4:
+                                {
+                                    Console.Write("\n\n\n\n");
+                       
+                                    Console.CursorLeft = 87;
+                                    Console.Write("DELETE CONTACT FROM ADDRESS BOOK ", Console.CursorLeft);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("******************************************************************", Console.CursorLeft);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Enter the first name of contact you wanted to remove : ");
+                                    string name = Convert.ToString(Console.ReadLine());
+                                    Person.deleteContact(name);
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("__________________________________________________________________");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Press any key to Main menu..");
+
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto Main_Menu;
+                                }
+                               
+                            case 5:
+                                {
+                                    Console.Write("\n\n\n\n");
+                       
+                                    Console.CursorLeft = 98;
+                                    Console.Write("DISPLAY ALL CONTACTS", Console.CursorLeft);
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("____________________________________________________________________________");
+                                    Console.Write("\n");
+                                    Person.PrintPerson();
+                                    Console.Write("\n");
+                                    Console.WriteLine();
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("____________________________________________________________________________");
+                                    Console.CursorLeft = 70;
+                                    Console.Write("Press any key to Main menu..");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto Main_Menu;
+                    }
+                            case 6:
+                                {
+                                    Console.Write("\n\n\n\n");
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("_______________________________________________________________________");
+                                    Console.Write("\n");
+                                    Console.CursorLeft = 87;
+                                    Console.WriteLine("THANK YOU FOR USING APPLICATION..!!!");
+                                    
+                                    Console.CursorLeft = 70;
+                                    Console.WriteLine("_______________________________________________________________________");
+                                    Console.Write("\n\n");
+                                }
+                            break;
+                                
+                            default:
+                                Console.Write("\n\n\n\n");
+                                Console.CursorLeft = 87;
+                                Console.WriteLine("Please enter valid option", Console.CursorLeft);
+                                Console.Write("\n");
+                                Console.CursorLeft = 87;
+                                Console.Write("Press any key to Main menu...", Console.CursorLeft);
+                                Console.ReadKey();
+                                Console.Clear();
+                                goto Main_Menu;
+                                
+                        }
+
+        }
+    }
+}
